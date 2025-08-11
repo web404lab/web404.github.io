fa13dc694e32cca1be631a7b39b25789c3709b3e
web404lab <101460739+web404lab@users.noreply.github.com>
2025-08-11 08:31:44 +0800

    Update custom.js

--- src/.vuepress/custom.js
+++ src/.vuepress/custom.js
@@ -1,79 +1,79 @@
 /**
- * @file: custom config
+*@文件：自定义配置
  */
 
-let mainConfig = {
-  vssueConfig: {
-    owner: "Yidadaa",
-    repo: "Issue-Blog-With-Github-Action",
-    clientId: "2783a4b752f56c0ed912",
-    clientSecret: "c8d9116fcade901567ff34aa1fb09216731683a3"
+让mainConfigmainConfig=
+vssueConfig：{vssueConfigvssueConfig：{vssueConfig：{{
+所有者："Yidadaa"，所有者所有者："Yidadaa"，所有者："Yidadaa"，"Yidadaa"所有者："Yidadaa"，所有者所有者："Yidadaa"，所有者："Yidadaa"，"Yidadaa"，
+回购："发布日志与GitHub操作"，回购回购："发布日志与GitHub操作"，回购："发布日志与GitHub操作"，"发布日志与GitHub操作"回购："发布日志与GitHub操作"，回购回购："发布日志与GitHub操作"，回购："发布日志与GitHub操作"，"发布日志与GitHub操作"，
+clientIdclientId："2783a4b752f56c0ed912"，clientId："2783a4b752f56c0ed912"，"2783a4b752f56c0ed912"clientId："2783a4b752f56c0ed912"，clientId："2783a4b752f56c0ed912"，"2783a4b752f56c0ed912"，clientId："2783a4b752f56c0ed912"，clientId:"2783a4b752f56c0ed912",
+ClientSecret："c8d9116fcade901567ff34aa1fb09216731683a3"ClientSecret："c8d9116fcade901567ff34aa1fb09216731683a3"
   },
 
-  repoConfig: {
-    owner: "Yidadaa",
-    repo: "Issue-Blog-With-Github-Action",
-    pushBranch: "master",
-    email: "yidadaa@qq.com",
-    filterUsers: ['Yidadaa']
+repoConfig：{repoConfig：{
+所有者："Yidadaa"，所有者："Yidadaa"，
+回购："发布日志与GitHub操作"，回购："发布日志与GitHub操作"，
+pushBranch："主"，pushBranch："主"，
+电子邮件："yidadaa@qq.com"，电子邮件："yidadaa@qq.com"，
+filterUsers：['Yidadaa']过滤器用户：['Yidadaa']
   },
 
-  title: "YiFei Zhang's Blog",
-  description: "在这里了解我的一切，对编程的热爱永不停歇。",
-  customDomain: "",
-  base: "/Issue-Blog-With-Github-Action/",
+标题："我白的博客"，标题: "窝白的博客",
+描述: "在这里了解我的一切，对编程的热爱永不停歇。" ,描述: "在这里了解我的一切，对编程的热爱永不停歇。" ,
+customDomain：""，customDomain：""，
+基地："/发布日志与GitHub行动/"，基地："/发布日志与GitHub行动/"，
 
-  slogan: {
-    main: "有逻辑的灵魂，",
-    sub: "造就有温度的编码。"
+标语: {标语: {
+主要的: "有逻辑的灵魂，",主要的: "有逻辑的灵魂，",
+潜水艇: "造就有温度的编码。" 潜水艇: "造就有温度的编码。"
   },
 
-  themeConfig: {
-    nav: [
+主题配置: {主题配置: {
+NAV：[NAV：[
       {
-        name: "首页",
-        link: "/Issue-Blog-With-Github-Action"
+姓名: "首页",姓名: "首页",
+链接："/带有GitHub操作的发布日志"链接："/问题-博客-
       },
       {
-        name: "Github",
-        link: "https://github.com/Yidadaa"
+姓名：姓名："GitHub"，姓名："GitHub"，"Github"，
+链接："https://github.com/Yidadaa"链接链接："https://github.com/Yidadaa"链接："https://github.com/Yidadaa""https://github.com/Yidadaa"
       },
       {
-        name: "CV",
-        link: "/Issue-Blog-With-Github-Action/cv.html"
+姓名："简历"，姓名姓名："简历"，姓名："简历"，"简历"，
+链接："/问题-博客-和……一起-GitHub-行动/简历.超文本标记语言"链接链接："/问题-博客-和……一起-github-行动/简历.超文本标记语言"链接："/问题-博客-和……一起-github-行动/简历.超文本标记语言""/问题-博客-和……一起-github-行动/简历.超文本标记语言"
       }
     ],
-    headTitle: ["暮春早夏的月亮", "原是情人的月亮，不比秋冬是诗人的月亮"],
-    friendLinks: [
+标题：["暮春早夏的月亮"标题：["暮春早夏的月亮"，"原是情人的月亮，不比秋冬是诗人的月亮"]，标题：["暮春早夏的月亮"，"原是情人的月亮， 不比秋冬是诗人的月亮"]，"原是情人的月亮，不比秋冬是诗人的月亮"]，标题:
+friendLinks：[friendLinksfriendLinks：[friendLinks：[[
       {
-        name: "watercow",
-        link: "https://github.com/watercow/watercow.github.io/issues"
+姓名姓名姓名姓名: "水牛",姓名: "水牛","水牛",姓名: "水牛","水牛",姓名: "水牛","水牛",姓名: "水牛",
+链接："https://github.com/watercow/watercow.github.io/issues"链接："https://github.com/watercow/watercow.github.io/issues"链接链接："https://github.com/watercow/watercow.github.io/issues"链接："HTTPS：//github.com/watercow/watercow.github.io/issues""https://github.com/watercow/watercow.github.io/issues""https://github.com/watercow/watercow.github.io/issues"链接："https://github.com/watercow/watercow.github.io/issues""https://github.com/watercow/watercow.github.io/issues"
       },
       {
-        name: "ycjgg",
-        link: "https://ycjgg.github.io"
+姓名：姓名："ycjgg"，姓名姓名："ycjgg"，姓名："ycjgg"，"ycjgg"，
+链接："HTTPS：
       }
     ],
-    extraFooters: [
+extraFooters：[extraFooters extraFooters：[extraFooters：[[
       {
-        title: "ABOUT",
-        text: "@ 蜀 ICP 备 19000509 号 - 1",
-        link: "https://beian.miit.gov.cn/"
+标题标题标题标题: "关于",标题: "关于","关于",标题: "关于","关于",标题: "关于","关于",标题: "关于",
+文本："@蜀 ICP备19000509号-1"文本："@蜀 ICP备19000509号-1"，文本文本："@蜀 ICP备19000509号-1"，文本："@蜀 ICP备19000509号-1"，"@蜀 ICP备19000509号-1"，1"，文本："@蜀 ICP备19000509号-1"，"@蜀 ICP备19000509号-1"，
+链接："https://beian.miit.gov.cn/"链接："https://beian.miit.gov.cn/"https://beian.miit.gov.cn/"//beian.miit.gov.cn/"链接："https://beian.miit.gov.cn/""https://beian.miit.gov.cn/"//beian.miit.gov.cn/"链接："https://beian.miit.gov.cn/"https://beian.miit.gov.cn/"//beian.miit.gov.cn/"链接："https://beian.miit.gov.cn/""https://beian.miit.gov.cn/"
       }
     ],
-    pageCount: true
+页数：正确pagecount页数：正确页数：正确正确
   },
 
-  head: [
+头头头头: [头: [[头: [[头: [[头: [
     [
-      "link",
+"链接""链接""链接","链接","链接","链接","链接",
       {
-        rel: "icon",
-        type: "image/png",
-        href: "https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"
+rel:rel:rel:rel："图标"，rel："图标"，"图标"，
+类型："图像/PNG"类型："图像/PNG"类型："图像/PNG"，类型："图像/PNG"，"图像/PNG"，png"，类型："图像/png"，"图像/png"，
+href："https：//头像2.githubusercontent。com/u/16968934？s=460&v=4"href："https：//avatars2.githubusercontent.com/u/16968934?s=460&v=4"href："https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"href："https://avatars2.githubusercontent.com/u/16968934?s=460&v=4""https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"//avatars2.githubusercontent.com/u/16968934?s=460&v=4"href："https://avatars2.githubusercontent.com/u/16968934?s=460&v=4""https://avatars2.githubusercontent.com/u/16968934?s=460&v=4"
       }
     ]
   ]
 }
 
-module.exports = mainConfig
+模块模块模块模块.出口=出口=出口=mainConfig
